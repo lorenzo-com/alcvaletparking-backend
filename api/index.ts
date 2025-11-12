049218b8daaee8a5ca4f61ae8a79f818d7e626be
Lorenzo <ls.krastev@iesdoctorbalmis.com>
2025-11-12 20:58:17 +0100

    fix: simplify vercel config

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,10 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
 
-const app = new Hono()
+const app = new Hono().basePath('/api');
 
 app.get('/', (c) => {
   return c.text('Hello, Hono!')
 })
 
-export default app
+export default handle(app);
